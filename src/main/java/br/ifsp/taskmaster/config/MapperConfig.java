4ba26d34fa51926b73879a896b209df3e62f3738
natalia <nataliadasilva.t@gmail.com>
2025-10-06 21:23:52 -0300

    feat: Add MapperConfig for ModelMapper configuration

---
+++ src/main/java/br/ifsp/taskmaster/config/MapperConfig.java
@@ -0,0 +1,14 @@
+package br.ifsp.taskmaster.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+}
